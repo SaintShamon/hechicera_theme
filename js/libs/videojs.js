bc5748df0207d2d9839faa8729ac8de102e34d32
modifikult <66362402+modifikult@users.noreply.github.com>
2022-11-30 19:43:34 +0200

    update videojs

--- js/libs/videojs.js
+++ js/libs/videojs.js
@@ -1,35 +1,40 @@
 import $ from 'jquery';
 
-var videoPlayer = videojs('#v-player', {
-    autoplay: 'muted',
-});
-
 $(document).ready(function () {
-    $('.video_block').each(function () {
-        const videoBtn = $(this).find('.video-btn');
+    const video = $('.video_block');
 
+    if (video.length) {
+        var videoPlayer = videojs('#v-player', {
+            autoplay: 'muted',
+        });
 
-        $(this).click(function () {
+        let videoBtn = video.find('.video-btn');
+
+        video.click(function () {
             if (videoBtn.hasClass('pause-button')) {
                 videoPlayer.pause();
-                videoBtn.removeClass('pause-button')
+                videoBtn.removeClass('pause-button');
             } else {
                 videoPlayer.play();
-                videoBtn.addClass('pause-button')
+                videoBtn.addClass('pause-button');
             }
         });
 
-        $(this).on('touchstart', function () {
+        video.on('touchend', function (e) {
             if (videoBtn.hasClass('pause-button')) {
-                console.log('nothave')
                 videoPlayer.pause();
-                videoBtn.removeClass('pause-button')
+                videoBtn.removeClass('pause-button');
             } else {
-                console.log('have')
                 videoPlayer.play();
-                videoBtn.addClass('pause-button')
+                videoBtn.addClass('pause-button');
             }
         });
-    });
+
+        videoBtn.on('touchstart', function (e) {
+            e.stopPropagation();
+            videoPlayer.play();
+            videoBtn.addClass('pause-button');
+        });
+    }
 });
 
