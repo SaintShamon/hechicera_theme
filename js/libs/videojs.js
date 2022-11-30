95597553b8ed44e1dd7744a6a6d53835ded24d88
modifikult <66362402+modifikult@users.noreply.github.com>
2022-11-30 15:24:17 +0200

    update video block

--- js/libs/videojs.js
+++ js/libs/videojs.js
@@ -1,6 +1,35 @@
-videojs('v-player', {
-    autoplay: false,
-    width: '100%',
-    height: '100%',
+import $ from 'jquery';
+
+var videoPlayer = videojs('#v-player', {
+    autoplay: 'muted',
+});
+
+$(document).ready(function () {
+    $('.video_block').each(function () {
+        const videoBtn = $(this).find('.video-btn');
+
+
+        $(this).click(function () {
+            if (videoBtn.hasClass('pause-button')) {
+                videoPlayer.pause();
+                videoBtn.removeClass('pause-button')
+            } else {
+                videoPlayer.play();
+                videoBtn.addClass('pause-button')
+            }
+        });
+
+        $(this).on('touchstart', function () {
+            if (videoBtn.hasClass('pause-button')) {
+                console.log('nothave')
+                videoPlayer.pause();
+                videoBtn.removeClass('pause-button')
+            } else {
+                console.log('have')
+                videoPlayer.play();
+                videoBtn.addClass('pause-button')
+            }
+        });
+    });
 });
 
--- template-parts/acf-blocks/video_block.php
+++ template-parts/acf-blocks/video_block.php
@@ -9,8 +9,11 @@ $video = get_sub_field('video');
 if ($video):
     ?>
     <section class="video_block" id="<?= esc_attr($id) ?>-section">
-        <video id="v-player">
+        <video id="v-player" height="100%">
             <source src="<?php echo $video; ?>">
         </video>
+        <div class="video-control-button">
+            <div class="video-btn"></div>
+        </div>
     </section>
 <?php endif; ?>
