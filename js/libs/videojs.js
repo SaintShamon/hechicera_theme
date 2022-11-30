286338040f45bd03f644674d4473db8bf409be91
Maksym <maxkoytsan@gmail.com>
2022-11-30 18:37:47 +0200

    fix video_block

--- js/libs/videojs.js
+++ js/libs/videojs.js
@@ -1,35 +1,40 @@
 import $ from 'jquery';
 
-var videoPlayer = videojs('#v-player', {
-    autoplay: 'muted',
-});
 
-$(document).ready(function () {
-    $('.video_block').each(function () {
-        const videoBtn = $(this).find('.video-btn');
-
-
-        $(this).click(function () {
-            if (videoBtn.hasClass('pause-button')) {
-                videoPlayer.pause();
-                videoBtn.removeClass('pause-button')
-            } else {
-                videoPlayer.play();
-                videoBtn.addClass('pause-button')
-            }
+$('.site').each(function (i, elem) {
+    if ($("section").hasClass("video_block")) {
+        var videoPlayer = videojs('#v-player', {
+            autoplay: 'muted',
         });
+        $(document).ready(function () {
 
-        $(this).on('touchstart', function () {
-            if (videoBtn.hasClass('pause-button')) {
-                console.log('nothave')
-                videoPlayer.pause();
-                videoBtn.removeClass('pause-button')
-            } else {
-                console.log('have')
-                videoPlayer.play();
-                videoBtn.addClass('pause-button')
-            }
+            $('.video_block').each(function () {
+                const videoBtn = $(this).find('.video-btn');
+
+
+                $(this).click(function () {
+                    if (videoBtn.hasClass('pause-button')) {
+                        videoPlayer.pause();
+                        videoBtn.removeClass('pause-button')
+                    } else {
+                        videoPlayer.play();
+                        videoBtn.addClass('pause-button')
+                    }
+                });
+
+                $(this).on('touchstart', function () {
+                    if (videoBtn.hasClass('pause-button')) {
+                        console.log('nothave')
+                        videoPlayer.pause();
+                        videoBtn.removeClass('pause-button')
+                    } else {
+                        console.log('have')
+                        videoPlayer.play();
+                        videoBtn.addClass('pause-button')
+                    }
+                });
+            });
+
         });
-    });
+    }
 });
-
