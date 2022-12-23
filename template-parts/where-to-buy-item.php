24efe88010d9769f2093d8ed94dfd52b4139669e
modifikult <66362402+modifikult@users.noreply.github.com>
2022-12-23 15:46:45 +0200

    update where-to-buy-item

--- template-parts/where-to-buy-item.php
+++ template-parts/where-to-buy-item.php
@@ -1,9 +1,16 @@
 <?php
 $url = get_field('add_shop_url');
+$shop_url_list = get_field('shop_url_list');
 $address = get_field('adress'); ?>
 
 <div class="list_item text--size--38 text--center">
     <h3><?php echo the_title(); ?></h3>
-    <?php echo $address; ?>
-    <span><?php echo $url; ?></span>
+    <?php if ($address) : ?>
+        <?php echo $address; ?>
+    <?php endif; ?>
+    <?php if ($shop_url_list) : ?>
+        <?php foreach ($shop_url_list as $shop_url) : ?>
+            <a href="<?php echo esc_url($shop_url['shop_url']['url']); ?>"><?php echo $shop_url['shop_url']['title']; ?></a>
+        <?php endforeach; ?>
+    <?php endif; ?>
 </div>
