13c3bab98fb569675da9659eeb8fe02bad7f1abb
Maksym <maxkoytsan@gmail.com>
2023-01-06 14:07:19 +0200

    fix awars slider

--- js/template-parts/blocks/awards_slider.js
+++ js/template-parts/blocks/awards_slider.js
@@ -18,11 +18,23 @@ function awardslider() {
                     infinite: false,
                     cssEase: 'linear',
                     speed: 500,
-                    slidesToShow: 2,
+                    slidesToShow: 3,
                     slidesToScroll: 1,
                     autoplay: false,
                     // focusOnSelect: true,
                     // autoplaySpeed: 5000,
+                    responsive: [{
+                            breakpoint: 361,
+                            settings: {
+                                slidesToShow: 2,
+                                slidesToScroll: 1,
+
+                            }
+                        }
+                        // You can unslick at a given breakpoint now by adding:
+                        // settings: "unslick"
+                        // instead of a settings object
+                    ]
                 });
 
             }
